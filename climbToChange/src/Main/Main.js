695dbea3b36d47fece98e9aecbc545e9e85daa25
Chung Min Kim <chungmin99@gmail.com>
2019-02-16 20:09:11 -0800

    Version w/ drawertab

---
+++ climbToChange/src/Main/Main.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+import { theme } from '../theme'
+
+export default class Main extends React.Component {
+    render() {
+        return ( 
+            <Text>Main</Text>
+        )
+    }
+}
---
+++ climbToChange/src/theme.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const theme = {
+    a: 'b'
+}
+export {
+    theme
+}
+
