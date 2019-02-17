9a8977ef79d68a0b1a6878bc42548710275e1ef0
Chung Min Kim <chungmin99@gmail.com>
2019-02-17 09:47:47 -0800

    Added title to main page

--- climbToChange/src/Main/Main.js
+++ climbToChange/src/Main/Main.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { StyleSheet, Text, View, ScrollView} from 'react-native';
 
 import { Block } from './Block'
+import { theme } from '../theme'
 import Entries from './data.json'
 
 export default class Main extends React.Component {
@@ -9,6 +10,7 @@ export default class Main extends React.Component {
         return ( 
             <ScrollView>
             <View>
+            <Text style={{fontSize: 40, textAlign: 'center', margin: 20}}>Climb To Change</Text>
             <Block box={Entries.box1}></Block>
             <Block box={Entries.box2}></Block>
             <Block box={Entries.box3}></Block>
