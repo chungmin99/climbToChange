6a2c1b5e1a0c87d31e1413449a55920305b175f6
Nicole Chui <nicolechui@Nicoles-MacBook-Pro-2.local>
2019-02-16 22:14:22 -0800

    input data and sorted the data based on score

---
+++ climbToChange/src/QAndA.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+class QAndA extends React.Component {
+  render() {
+    console.log(this.props);
+    return (
+      <View>
+        style=
+        {{
+          flex: 1,
+          borderWidth: 1,
+          justifyContent: "center",
+          alignItems: "center",
+          backgroundColor: "yellow"
+        }}
+        }); return ({this.props}
+        );
+      </View>
+    );
+  }
+}
+
+export default QAndA;
