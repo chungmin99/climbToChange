8b6c27c6efbe72e3461248deaa800076a8102825
winnie3269 <User@MacBook-Pro-4.local>
2019-02-16 22:28:57 -0800

    working on main.js

--- climbToChange/src/main.js
+++ climbToChange/src/main.js
@@ -1,1 +1,35 @@
-<
+import React, {Component} from 'react'
+import { AppRegistry, Text, View } from 'react-native';
+import { SearchBar } from 'react-native-elements';
+import image from './ocean.jpg';
+
+var styles = {
+  backgroundImage: 'url('+image+')'
+};
+
+export default class Main extends React.Component {
+  state = {
+    search: '',
+  };
+
+  updateSearch = search => {
+    this.setState({ search });
+  };
+
+  render() {
+    const { search } = this.state;
+
+    return (
+      <View>
+        <Text>This is the main menu.</Text>
+      </View>
+      <SearchBar
+        placeholder = "Search"
+        onChangeText = { this.updateSearch }
+        value = { search }
+      />
+    );
+  }
+}
+
+AppRegistry.registerComponent('Main', () => Main);
